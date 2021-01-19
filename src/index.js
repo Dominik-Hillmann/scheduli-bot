c6f2e559bb2206b3387a8ef6ccff05a7e6033fe4
Dominik-Hillmann <dominik.hillmann@gmx.de>
2021-01-20 00:07:59 +0100

    Index done

--- src/index.js
+++ src/index.js
@@ -3,5 +3,28 @@ require('dotenv').config();
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+const chrono = require('chrono-node');
+
 console.log(process.env.TOKEN);
+console.log(chrono.parseDate('The fourth of Jul 1776'));
+
+
+client.on('ready', () => console.log('redy'));
+client.on('message', (msg) => {
+    let isBot = msg.author.bot;
+    let calledFor = msg.content.includes('Moin');
+    
+    if (isBot || !calledFor) {
+        return;
+    }
+
+    msg.channel.send('Das ist ein Test');
+
+    let parsedDate = chrono.parseDate(msg.content);
+    console.log(parsedDate);
+    if (parsedDate !== null) {
+        msg.channel.send(parsedDate.toString());
+    }
+});
+
 client.login(process.env.TOKEN);
