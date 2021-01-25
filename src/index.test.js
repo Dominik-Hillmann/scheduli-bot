c95ea6088112e28c2a2eefcc2f8a87a36dcb4dba
Dominik-Hillmann <dominik.hillmann@gmx.de>
2021-01-25 15:06:51 +0100

    Installed Jest

---
+++ src/index.test.js
@@ -0,0 +1,10 @@
+
+
+describe("forgotPassword()", () => {
+    it("should return true", () => {
+        //Testing a boolean
+        expect(true).toBeTruthy();
+        //Another way to test a boolean
+        expect(true).toEqual(false);
+    });
+});
