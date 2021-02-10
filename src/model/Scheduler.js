a43badab9b567cfe53079f5eb51a04c6597618c1
Dominik-Hillmann <dominik.hillmann@gmx.de>
2021-02-10 21:53:23 +0100

    Started scheduler

--- src/model/Scheduler.js
+++ src/model/Scheduler.js
@@ -9,12 +9,24 @@ export class Scheduler {
      * 
      * @param {Map<string, import('./TimeFrame').TimeFrame[]>} userSchedules 
      * @param {number} minSecsInCommon
+     * @throws {TypeError} If there are schedules for only one 
+     * user.
      */
     constructor(userSchedules, minSecsInCommon = -1) {
         // für jede Kombination von Zeiträumen
         // Zeichne auf, wieviele Zeiträume überlappen
-        this.test = userSchedules;
+        // 
 
+        const userIds = Object.keys(userSchedules);
+        if (userIds.length <= 1) {
+            throw new TypeError(`Need more than one user to schedule. Got ${userIds.length}.`);
+        }
+
+        // let maxIndex = 
+
+        // for (let i = 1; i < userIds.length; i++) {
+
+        // }
     }
 
 
